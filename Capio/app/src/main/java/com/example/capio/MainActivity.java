c58647b016a7d0857a3fd1a52ebb7de697e8e4e7
tomvh123 <thaaster@avans.nl>
2017-05-04 10:02:33 +0200

    interfaces

--- Capio/app/src/main/java/com/example/capio/MainActivity.java
+++ Capio/app/src/main/java/com/example/capio/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.capio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_agenda);
+        setContentView(R.layout.activity_inlog);
 
-    }}
+        button = (Button) findViewById(R.id.ButtonInloggen);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
 
 
 
